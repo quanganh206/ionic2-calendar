0fd8fb1d152e628ed1e6d1612c817e8935237676
Ionitron <hi@ionicframework.com>
2016-08-05 22:42:37 +0700

    update calendar directive

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,10 +1,8 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
-import {CalendarComponent} from '../../directive/calendar/calendar'; 
 
 @Component({
   templateUrl: 'build/pages/home/home.html',
-  directives: [CalendarComponent],
 })
 export class HomePage {
   constructor(private navCtrl: NavController) {
